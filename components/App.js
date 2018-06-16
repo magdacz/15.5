c65b289322d4a732260aa6299c8ba3eb2181ea60
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2018-06-16 19:48:20 +0200

    Update

--- components/App.js
+++ components/App.js
@@ -24,31 +24,11 @@ handleSearch: function(searchingText) {
     }.bind(this));
   },
     
-/*    
-getGif: function(searchingText, callback) {  
-    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  
-    var xhr = new XMLHttpRequest();  
-    xhr.open('GET', url);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-           var data = JSON.parse(xhr.responseText).data; 
-            var gif = {  
-                url: data.fixed_width_downsampled_url,
-                sourceUrl: data.url
-            };
-            callback(gif);  
-        }
-    };
-    xhr.send();
-},
-*/
-    
     getGif: function(searchingText) {  
         return new Promise (
             function (resolve, reject) {
                 var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  
                 var xhr = new XMLHttpRequest();  
-                xhr.open('GET', url);
                 xhr.onload = function() {
                     if (xhr.status === 200) {
                         var data = JSON.parse(xhr.responseText).data; 
@@ -61,6 +41,7 @@ getGif: function(searchingText, callback) {
                         reject(new Error(this.statusText));
                     }
             };
+            xhr.open('GET', url);
             xhr.send();
         }
     )
